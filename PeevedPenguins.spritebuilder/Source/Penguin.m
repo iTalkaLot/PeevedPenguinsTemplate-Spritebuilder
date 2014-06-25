999efd4f254c041335a75cbaa82fefaac76b0b1b
iTalkaLot <ishanithakur@Aniruddhas-MacBook-Pro.local>
2014-06-25 08:49:26 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Penguin.m
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -2,7 +2,7 @@
 //  Penguin.m
 //  PeevedPenguins
 //
-//  Created by Ishani Thakur on 6/24/14.
+//  Created by Ishani Thakur on 6/25/14.
 //  Copyright (c) 2014 Apportable. All rights reserved.
 //
 
